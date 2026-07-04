5e93b161a46bce57c8107aba544071f0ee48b096
Johnny-Fingers <jrbramos05@gmail.com>
2026-07-04 17:54:40 -0300

    update(config): Update traceability and workers config

--- playwright.config.ts
+++ playwright.config.ts
@@ -9,15 +9,15 @@ export default defineConfig({
   fullyParallel: true,               /* Enable parallel processing to speed up execution */
   forbidOnly: !!process.env.CI,      /* Prevent tests containing .only from being uploaded to CI */
   retries: process.env.CI ? 2 : 0,   /* Retry twice on GitHub Actions if the test fails */
-  workers: process.env.CI ? 2 : undefined, /* Monitor the threads in CI to avoid overloading the agent */
+  workers: process.env.CI ? 2 : "50%", /* Monitor the threads in CI to avoid overloading the agent */
   reporter: [
     ["html", { open: "never" }],     /* Generate an HTML report without attempting to open it automatically */
     ["list"]                         /* Display a clean log in the terminal */
   ],
   use: {
-    trace: 'retain-on-failure',             /* Record detailed traces only if the test fails */
-    screenshot: 'only-on-failure',          /* Take screenshots if the test fails */
-    video: 'on-first-retry',                /* Record a video if the test goes into a retry loop */
+    trace: "retain-on-failure",             /* Record detailed traces only if the test fails */
+    screenshot: "only-on-failure",          /* Take screenshots if the test fails */
+    video: "retain-on-failure",                /* Record a video if the test fails */
   },
 
   projects: [
