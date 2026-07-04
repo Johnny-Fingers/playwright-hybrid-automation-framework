87699f2ced5741e1818cac83bf5887a2da1bb4d2
Johnny-Fingers <jrbramos05@gmail.com>
2026-07-04 12:11:49 -0300

    update: Define multi-projects by separating the UI and API baseURL

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,17 +1,20 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
+
+const uiCommonOptions = {
+  baseURL: "https://demo.realworld.show" /* Base URL for the public UI */
+}
 
 export default defineConfig({
-  testDir: './tests',
+  testDir: "./tests",
   fullyParallel: true,               /* Enable parallel processing to speed up execution */
   forbidOnly: !!process.env.CI,      /* Prevent tests containing .only from being uploaded to CI */
   retries: process.env.CI ? 2 : 0,   /* Retry twice on GitHub Actions if the test fails */
   workers: process.env.CI ? 2 : undefined, /* Monitor the threads in CI to avoid overloading the agent */
   reporter: [
-    ['html', { open: 'never' }],     /* Generate an HTML report without attempting to open it automatically */
-    ['list']                         /* Display a clean log in the terminal */
+    ["html", { open: "never" }],     /* Generate an HTML report without attempting to open it automatically */
+    ["list"]                         /* Display a clean log in the terminal */
   ],
   use: {
-    baseURL: 'https://api.realworld.show', /* Base URL for the public UI */
     trace: 'retain-on-failure',             /* Record detailed traces only if the test fails */
     screenshot: 'only-on-failure',          /* Take screenshots if the test fails */
     video: 'on-first-retry',                /* Record a video if the test goes into a retry loop */
@@ -19,12 +22,29 @@ export default defineConfig({
 
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "api-tests",
+      testDir: "./tests/api",
+      use: {
+        baseURL: "https://api.realworld.show" /* Base URL for the public API */
+      }
     },
+    // Running tests on Desktop Chrome
     {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      name: "ui-chrome",
+      testDir: "./tests/ui",
+      use: {
+        ...devices["Desktop Chrome"],
+        ...uiCommonOptions
+      }
     },
+    // Running tests on Desktop Firefox
+    {
+      name: "ui-firefox",
+      testDir: "./tests/ui",
+      use: {
+        ...devices["Desktop Firefox"],
+        ...uiCommonOptions
+      }
+    }
   ],
 });
